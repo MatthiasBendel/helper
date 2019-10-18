833102d73b14c66279a84381c4ab948dd1de8554
MatthiasBendel <matthiasbendel@gmail.com>
2019-10-18 15:16:03 +0200

    Fixed Update Procedure

--- updater.py
+++ updater.py
@@ -17,11 +17,11 @@ def get_online_version():
     h = httplib2.Http(".cache")
     resp, content = h.request(ONLINE_VERSION_FILE, "GET")
     online_version = str(content, 'utf-8')[14:]
-    return version.__version__ == float(online_version), online_version
+    return online_version
 
 
 def update():   # remove old installation and copy the updated version again to the same path
-    installation_path = os.path.realpath(sys.argv[0])
+    installation_path = os.path.realpath(sys.argv[0])[:-9]
     path, file = install.download_zip()
     install.extract_zip(installation_path, path + file)
     # todo offer git update
@@ -29,7 +29,8 @@ def update():   # remove old installation and copy the updated version again to 
 
 def check_for_update():
     try:
-        (is_up_to_date, online_version) = get_online_version()
+        online_version = get_online_version()
+        is_up_to_date = version.__version__ >= float(online_version)
         if is_up_to_date:
             print("This is the actual version ({}).".format(version.__version__))
         else:
